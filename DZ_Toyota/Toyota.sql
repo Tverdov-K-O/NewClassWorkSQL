87004e2424a622131807fce2aedd7c43d2ee8535
tverdov.konstantin tverdov.konstantin <konstantin08081997@mail.ru>
2022-01-24 16:19:41 +0200

    24.01.2022

---
+++ DZ_Toyota/Toyota.sql
@@ -0,0 +1,14 @@
+﻿SELECT
+	Models.Name as [Марка],
+	Modifications.Name as [Модификация],
+	Engines.Name as [Двигатель],
+	Transmissions.Name as [Трансмиссия],
+	Color.Name as [Цвет]
+FROM
+	Models
+JOIN Modifications ON Modifications.ModelId = Models.Id
+JOIN ColorModification ON ColorModification.ModificationId = Modifications.Id
+JOIN Color ON Color.Id = ColorModification.ColorId
+JOIN EngineModification ON EngineModification.ModificationId = Modifications.Id
+JOIN Engines ON Engines.Id = EngineModification.EngineId
+JOIN Transmissions ON Transmissions.Id = Engines.TransmissionId
